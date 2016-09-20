fd055ea36999f1cb50c8fbf058a881b9cbabd155
Peter Anderson <diniremix@gmail.com>
2016-09-19 22:58:05 -0500

    remove, var 'port', added various 'Content-Type' to setHeader

--- spider.php
+++ spider.php
@@ -14,7 +14,6 @@ class Spider {
 	private $response_code = null;
 	private $response_message = null;
 	private $response_error = null;
-	private $port = null;
 	private $curl = null;
 
 	public function __construct($url, $method = 'GET') {
@@ -26,6 +25,7 @@ class Spider {
 	public function getRequest() {
 		return $this->response_request;
 	}
+
 	public function getHeaderLine($line=null) {
 		// see http://php.net/manual/es/function.curl-getinfo.php
 		// for more info
@@ -55,10 +55,26 @@ class Spider {
 	}
 
 	public function setHeader($headers) {
-		if($headers==='default'){
-			$this->headers = array("content-type: application/json");
-		}else{
-			$this->headers = array($headers);
+		switch ($headers) {
+			case 'default':
+			case 'json':
+				$this->headers = array("Content-Type: application/json");
+				break;
+			case 'xml':
+				$this->headers = array("Content-Type: application/xml");
+				break;
+			case 'x-www-form-urlencoded':
+			case 'form-urlencoded':
+			case 'form':
+				$this->headers = array("Content-Type: application/x-www-form-urlencoded");
+				break;
+			case 'plain':
+			case 'text/plain':
+				$this->headers = array("Content-Type: text/plain");
+				break;
+			default:
+				$this->headers = array($headers);
+				break;
 		}
 	}
 
@@ -91,6 +107,7 @@ class Spider {
 			)
 		);
 	}
+
 	public function send() {
 		$this->constructRequest();
 		$this->response_body = curl_exec($this->curl);
