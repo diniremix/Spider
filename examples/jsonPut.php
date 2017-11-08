ef8034d098bc65a5b42ced731635813649bd6f98
Jorge Brunal <jorge.brunal@gmail.com>
2017-11-08 02:48:21 -0500

    json PUT example

---
+++ examples/jsonPut.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once ('../spider.php');
+
+$req = new Spider("http://httpbin.org/put", "PUT");
+
+$req->setHeader("default");
+$req->addHeaders("Authorization: Bearer ExampleBearerToken");
+
+$req->send();
+
+header('Content-type: application/json');
+
+if ($req->hasError()){
+	echo json_encode(array(
+		'getStatusCode' => $req->getStatusCode(),
+		'message' => $req->getErrorMessage(),
+		'body' => $req->getBody(),
+		'getRequest' => $req->getRequest()
+	));
+}else{
+	echo json_encode(array(
+		'getStatusCode' => $req->getStatusCode(),
+		'body' => $req->getBody(),
+		'getHeaderLine' => $req->getHeaderLine(),
+		'getHeaderLine primary_ip ' => $req->getHeaderLine('primary_ip'),
+		'getHeaders' => $req->getHeaders(),
+		'getErrorMessage' => $req->getErrorMessage(),
+		'getRequest' => $req->getRequest()
+	));
+}
